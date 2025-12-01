6baa443b4af7c5421e5a668e54f7b30d8c3a4170
chubinho <andrewchubinets07@yandex.ru>
2025-12-01 20:57:07 +0300

    add data type process and modification of pytest

--- src/sort.py
+++ src/sort.py
@@ -1,9 +1,17 @@
-def bubble_sort(a: list) -> list:
+def bubble_sort(a: list[int]) -> list[int]:
     """Уставнавливаем флаг, который указывает, что
     перестановка была за проход,
     после этого проходимся по элементам и сравниваем два
     соседних элемента
     """
+    if not isinstance(a, list):
+        raise TypeError("Input should be a list")
+    if len(a) < 2:
+        return a
+    if not all(isinstance(x,int) for x in a):
+        raise TypeError("Each element must be an integer")
+    if any(x < 0 for x in a):
+        raise ValueError("Each element must be more than 0")
     sp = a[:]
     flag = True
     while flag:
@@ -12,10 +20,11 @@ def bubble_sort(a: list) -> list:
             if sp[i] > sp[i+1]:
                 sp[i], sp[i+1] = sp[i+1], sp[i]
                 flag = True
+
     return sp
 
 
-def quick_sort(a: list) -> list:
+def quick_sort_int(a: list[int]) -> list[int]:
     """
     Выбираем последний элемент, после чего
     создаем 3 списка: элементы меньшие нашего элемента,
@@ -23,6 +32,14 @@ def quick_sort(a: list) -> list:
     рекурсивно вызываем быструю сортировку уже
     для элементов, меньнших и больших нашего числа
     """
+    if len(a) < 2:
+        return a
+    if not isinstance(a, list):
+        raise TypeError("Input should be a list")
+    if not all(isinstance(x, int) for x in a):
+        raise TypeError("Each element must be an integer")
+    if any(x < 0 for x in a):
+        raise ValueError("Each element must be more than 0")
     sp = a[:]
     if len(sp) <= 1:
         return sp
@@ -30,10 +47,36 @@ def quick_sort(a: list) -> list:
     left_pivot = [x for x in sp if x < pivot]
     middle_pivot = [x for x in sp if x == pivot]
     right_pivot = [x for x in sp if x > pivot]
-    return quick_sort(left_pivot) + middle_pivot + quick_sort(right_pivot)
 
+    return quick_sort_int(left_pivot) + middle_pivot + quick_sort_int(right_pivot)
 
-def counting_sort(a: list) -> list:
+def quick_sort_float(a: list[float]) -> list[float]:
+    """
+    Выбираем последний элемент, после чего
+    создаем 3 списка: элементы меньшие нашего элемента,
+    равные ему, и большие его. После чего
+    рекурсивно вызываем быструю сортировку уже
+    для элементов, меньнших и больших нашего числа
+    """
+    if len(a) < 2:
+        return a
+    if not isinstance(a, list):
+        raise TypeError("Input should be a list")
+    if not all(isinstance(x, float) for x in a):
+        raise TypeError("Each element must be a float")
+    if any(x < 0 for x in a):
+        raise ValueError("Each element must be more than 0")
+    sp = a[:]
+    if len(sp) <= 1:
+        return sp
+    pivot = sp[-1]
+    left_pivot = [x for x in sp if x < pivot]
+    middle_pivot = [x for x in sp if x == pivot]
+    right_pivot = [x for x in sp if x > pivot]
+
+    return quick_sort_float(left_pivot) + middle_pivot + quick_sort_float(right_pivot)
+
+def counting_sort(a: list[int]) -> list[int]:
     """Создаем массив состоящий из нулей, после чего
     добавляем в него по индексу
     (индекс это текущее значение - минимальное число),
@@ -43,6 +86,12 @@ def counting_sort(a: list) -> list:
     """
     if len(a) < 1:
         return []
+    if not isinstance(a, list):
+        raise TypeError("Input should be a list")
+    if not all(isinstance(x, int) for x in a):
+        raise TypeError("Each element must be an integer")
+    if any(x < 0 for x in a):
+        raise ValueError("Each element must be more than 0")
     sp = a[:]
     max_sp = max(sp)
     min_sp = min(sp)
@@ -62,6 +111,14 @@ def radix_sort(a: list[int], base: int = 10) -> list[int]:
     больше 0, то сортируем по последнему числу и затем сортируем по самому
     левому числу массива
     """
+    if not isinstance(a, list):
+        raise TypeError("Input should be a list")
+    if not all(isinstance(x, int) for x in a):
+        raise TypeError("Each element must be an int")
+    if any(x < 0 for x in a):
+        raise ValueError("Each element must be more than 0")
+    if base < 2:
+        raise ValueError("Base must be >= 2")
     sp = a[:]
     if not sp:
         return sp
@@ -86,6 +143,12 @@ def bucket_sort(a: list[float], buckets: int | None = None) -> list[float]:
     после этого каждая корзина сортируется с помощью
     quick_sort и дополняет список вывода.
     """
+    if not isinstance(a, list):
+        raise TypeError("Input must be a list")
+    if not all(isinstance(x, (int, float)) for x in a):
+        raise TypeError("Each element must be a float")
+    if any(not (0 <= x < 1) for x in a):
+        raise ValueError("The value in bucket_sort should be in [0, 1)")
     if len(a) < 2:
         return a[:]
     arr = a[:]
@@ -105,7 +168,7 @@ def bucket_sort(a: list[float], buckets: int | None = None) -> list[float]:
     result = []
     for bucket in buckets_list:
         if len(bucket) > 1:
-            result.extend(quick_sort(bucket))
+            result.extend(quick_sort_float(bucket))
         else:
             result.extend(bucket)
 
@@ -138,6 +201,12 @@ def heap_sort(a: list[int]) -> list[int]:
     поэтому с помощью них мы поднимаемся всё выше
     и выше, формируя max-heap
     """
+    if not isinstance(a,list):
+        raise TypeError("Input must be a list")
+    if not all(isinstance(x,int) for x in a):
+        raise TypeError("Each element must be an int")
+    if any(x < 0 for x in a):
+        raise ValueError("Each element must be more than 0")
     sp = a[:]
     n = len(sp)
 
